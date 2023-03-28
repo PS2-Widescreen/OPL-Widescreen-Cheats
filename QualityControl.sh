75409f393fa7b37cbce4a433936078370b194252
Matías Israelson <57065102+israpps@users.noreply.github.com>
2023-03-27 23:06:13 -0300

    unify all the scripts into `QualityControl.sh`

--- QualityControl.sh
+++ QualityControl.sh
@@ -1,24 +1,53 @@
 #!/bin/bash
+
+
+for i in $@
+do 
+  if [ "$i" = "--help" ]; then
+	echo "available switches:"
+	echo "--no-case-normalize : dont normalize case of cheat codes"
+	exit
+  fi
+
+  if [ "$i" = "--no-case-normalize" ]; then
+	echo " -- cheat codes case will NOT be normalized"
+	NORMALIZE = 1
+  fi
+done
+
 RETURN_CODE=0
+LOL=0
 cd CHT
 for a in *.cht
 do
   HEAD=""
   HEAD=$(head -n1 -q "$a" | grep -Eo [0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z]_[0-9][0-9][0-9].[0-9][0-9])
+  COUNT=$(grep -iEo "^[0-9A-Fa-f]{8} [0-9A-Fa-f]{8}" "$a" | wc -l)
+  printf "\r[$a]:"
   if [ -z "$HEAD" ]; then
-    echo "[$a]: could not find ELF ID on header"
-    RETURN_CODE=1
-  fi
-
-  if [ "$HEAD.cht" != "$a" ]; then
-    echo "[$a]: has an invalid ELF header: [$HEAD]"
+    echo " could not find ELF ID on header"
     RETURN_CODE=1
   fi
   
+  if [ NORMALIZE != 1 ]; then
+    sed -Ei 's/[a-fA-F0-9]{8} [a-fA-F0-9]{8}/\L&/g' "$a"
+  fi
+  
+
+  if [ "$HEAD.cht" != "$a" ]; then
+    echo " has an invalid ELF header: [$HEAD]"
+    RETURN_CODE=1
+  fi
+
+  if [ $COUNT -gt 255 ]; then
+    echo " $COUNT Cheats detected, while OPL Cheat engine only supports a total of 255 cheats..." ;
+    RETURN_CODE=1
+  fi
+
   if  grep -q "Mastercode" "$a" ; then
     :
   else
-    echo "[$a]: could not find a matching mastercode string" ;
+    echo " could not find a matching mastercode string" ;
     RETURN_CODE=1
   fi
 done
--- lowercasecheats.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd CHT/
-for a in *.cht; do sed -Ei 's/[a-fA-F0-9]{8} [a-fA-F0-9]{8}/\L&/g' "$a"; done
