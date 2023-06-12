34b83c5c4f938df7ecfd08ff46c2f10eeb276760
Matías Israelson <57065102+israpps@users.noreply.github.com>
2023-06-12 11:54:17 -0300

    create automated update script
    [skip actions]

---
+++ update.bat
@@ -0,0 +1,37 @@
+@echo off
+title automated archive updater
+
+echo # listing widescreen hacks from pcsx2_patches...
+if not exist LST.TXT findstr /M "Widescreen" ..\pcsx2_patches\patches\*.*>LST.TXT
+REM busybox.exe grep -Eo "[a-zA-Z]{4}-[0-9]{5}.*.pnach" LST.TXT | busybox.exe grep -Eo "[a-zA-Z]{4}-[0-9]{5}" > LST2.TXT
+if not exist LST2.TXT (
+    echo ## Temporal list of game ID not found, constructing. this will take a while...
+    for /f %%g in ('busybox.exe grep -Eo "[a-zA-Z]{4}-[0-9]{5}.*.pnach" LST.TXT') do (
+        for /f %%x in ('echo %%g ^| busybox.exe grep -Eo "[a-zA-Z]{4}-[0-9]{5}" ^| busybox.exe sed "s/-/_/g" ^| busybox.exe sed "s/./&./8"') do (
+            echo %%x;%%g>>LST2.TXT
+        )
+    )
+)
+
+echo # Checking cheats missing in this repo with available mastercode...
+del FINAL.TXT
+set /a CNT=0
+for /f "delims=; tokens=1,*" %%a in (LST2.TXT) do (
+   if not exist CHT\%%a.cht (
+       if exist ..\Bare-Mastercodes-bin\MASTERCODES\%%a.cht (
+           echo %%a;%%b >>FINAL.TXT
+           set /a CNT += 1
+       )
+   )
+)
+echo found %CNT% candidates...
+
+for /f "delims=; tokens=1,*" %%a in (FINAL.TXT) do (
+    if exist ..\pcsx2_patches\patches\%%b (
+        copy ..\Bare-Mastercodes-bin\MASTERCODES\%%a.cht CHT\%%a.cht>nul
+        PS2_pnach_converter.exe ..\pcsx2_patches\patches\%%b -g >>CHT\%%a.cht
+    ) else echo HUSTON, WE HAVE A PROBLEM %%a @ %%b
+    PAUSE
+)
+REM del LST.TXT
+REM del LST2.TXT
