deceb28394b50414ba6281494919ab261449dc82
Matías Israelson <57065102+israpps@users.noreply.github.com>
2023-07-27 11:02:05 -0300

    update Quality Control script to be less verbose

--- QualityControl.sh
+++ QualityControl.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+C_RED='\033[1;31m'
+C_GREEN='\033[1;32m'
+C_YELLOW='\033[1;33m'
+C_BLUE='\033[1;34m'
+C_NC='\033[0m' # No Color
 
 for i in $@
 do 
@@ -24,9 +29,9 @@ do
   HEAD=$(head -n1 -q "$a" | grep -Eo [0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z]_[0-9][0-9][0-9].[0-9][0-9])
   COUNT=$(grep -iEo "^[0-9A-Fa-f]{8} [0-9A-Fa-f]{8}" "$a" | wc -l)
   MASTERCODE_COUNT=$(grep -iEo "^9[0-9A-Fa-f]{7} [0-9A-Fa-f]{8}" "$a" | wc -l)
-  printf "\r[$a]:"
+#  printf "\r[$a]:"
   if [ -z "$HEAD" ]; then
-    echo " could not find ELF ID on header"
+    echo -e "${C_YELLOW} CHT/$a: could not find ELF ID on header${C_NC}"
     RETURN_CODE=1
   fi
   
@@ -36,24 +41,29 @@ do
   
 
   if [ "$HEAD.cht" != "$a" ]; then
-    echo " has an invalid ELF header: [$HEAD]"
+    echo "${C_YELLOW} CHT/$a: has an invalid ELF header: [$HEAD]${C_NC}"
     RETURN_CODE=1
   fi
 
   if [ $COUNT -gt 250 ]; then
-    echo " $COUNT Cheats detected, OPL Cheat engine supports a max of 250 common cheats..." ;
+    echo -e "${C_YELLOW} CHT/$a: $COUNT Cheats detected, OPL Cheat engine supports up to 250 common cheats...${C_NC}" ;
+    RETURN_CODE=1
+  fi
+
+  if [ $COUNT -le 0 ]; then
+    echo -e "${C_RED} CHT/$a: $COUNT Cheats found!${C_NC}" ;
     RETURN_CODE=1
   fi
 
   if [ $MASTERCODE_COUNT -le 0 ] || [ $MASTERCODE_COUNT -gt 5 ]; then
-    echo " $MASTERCODE_COUNT Mastercodes. quantity out of range (1-5)..." ;
+    echo -e "${C_RED} CHT/$a: $MASTERCODE_COUNT Mastercodes. quantity out of range (1-5)...${C_NC}" ;
     RETURN_CODE=1
   fi
 
   if grep -q "Mastercode" "$a" ; then
     :
   else
-    echo " could not find a matching mastercode string" ;
+    echo -e "${C_RED} CHT/$a: could not find a matching mastercode string${C_NC}" ;
     RETURN_CODE=1
   fi
 done
